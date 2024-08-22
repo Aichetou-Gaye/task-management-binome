55632cac65f902b8dd6e551e86cf68bed44aa3f3
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-08-22 14:44:14 +0100

    commit App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
- 
+ <div class="container-fluid p-0">
+    <nav class="navbar navbar-expand-lg navbar-primary bg-primary text-white mb-4">
+      <a class="navbar-brand text-white ms-3" href="#"><img src="./assets/logo.png" alt="Logo"></a>
+      <div class="collapse navbar-collapse d-flex justify-content-end">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link text-white" to="/taches">Tâches</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link text-white" to="/projet">Projets</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+
+    <router-view />
+  </div>
 </template>
 
 <script setup>
-import Tache from './components/taches/Tache.vue'
+import { useGestionStore} from '@/stores'
+
+const store = useGestionStore()
+
 </script>
 
-<template>
- <Tache />
-</template>
 
 <style scoped>
 
