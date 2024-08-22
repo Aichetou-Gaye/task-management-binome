28e585b58eafde46375462126740e68ea6cfc4ee
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-08-22 11:41:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Taches from './components/Taches.vue'
+</script>
+
+<template>
+ <Taches />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    Welcome
+</template>
+
+<script>
+
+</script>
